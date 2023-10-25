8c3b58d401dc8b31a9ebed928b19651fa92ffb76
Oussema-mzoughi <oussema.mzoughi@esprit.tn>
2023-10-25 10:01:05 +0100

    last fix

--- resume-service2/src/main/java/com/example/resumeservice2/ResumeService2Application.java
+++ resume-service2/src/main/java/com/example/resumeservice2/ResumeService2Application.java
@@ -2,9 +2,9 @@ package com.example.resumeservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-
+@EnableEurekaClient
 public class ResumeService2Application {
 
     public static void main(String[] args) {
